3949ef4ecd7311cb682579c6e5dcef696ab33125
viktor0502 <vik.more0502@gmail.com>
2022-10-13 13:29:56 +0300

    add adaptive

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const swiper = new Swiper('.swiper', {
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 10,
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+    breakpoints: {
+        // when window width is >= 320px
+        320:{
+          slidesPerView: 1,
+        },
+        500: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        769: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        
+      }
   });
